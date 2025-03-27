d01b984906e14d535cddbc3c58ffd4dfef83c653
rodolphe <rodolphefrancotte18@gmail.com>
2025-03-27 08:58:11 +0100

    Initial Commit

---
+++ WeatherTodaySwift/WeatherTodaySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTodaySwiftApp.swift
+//  WeatherTodaySwift
+//
+//  Created by Rodolphe on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTodaySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
